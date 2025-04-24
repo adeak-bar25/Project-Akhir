efb426076879b79cfa0bc62791b0baddbe71476e
Ade Akbar Magri Dinata <adeakbarmagridinata28@outlook.co.id>
2025-04-25 01:57:45 +0800

    Form dari Frontend sudah dapat diterima di Backend, Menambahkan responsive css

--- app.js
+++ app.js
@@ -3,39 +3,53 @@ const fs = require('fs');
 
 const app = express();
 app.use(express.static('public'));
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.set('view engine', 'hbs');
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 function generateRandomDigit(){
     return Math.floor(100000 + Math.random() * 900000);
-}
+};
 
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
 
 app.get('/new', (req, res) => {
-    res.render('new')
-})
+    res.render('new');
+});
 
 app.get('/join',(reg, res) => {
-    res.render('join')
-})
+    res.render('join');
+});
 
 app.get('/dashboard',(reg, res) => {
-    res.render('dashboard')
+    res.render('dashboard');
+});
+
+app.post('/newsession', (req, res) => {
+    console.log(req.body);
+    res.send('ok');
+    console.log(generateEventJSON(req.body.eventName, req.body.password, generateRandomDigit()));
 })
 
 app.use((req, res) => {
     res.status(404).render('404');
 })
 
+function generateEventJSON(eventName, passwordHash, code) {
 
-app.post('/new', (req, res) => {
-    const newCode = generateRandomDigit()
-    const codeElement =`<div class='code'>${newCode}</div>`
-    res.render('panitiamain', {code: newCode});
-})
+    return {
+        eventName: eventName,
+        passwordHash: passwordHash,
+        code: code,
+        feedback: []
+    };
+}
+
+
+
 
 app.listen(port, () => {
     console.log(`Aplikasi berjalan di http://localhost:${port}`);
