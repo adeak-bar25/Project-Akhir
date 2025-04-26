65a3ba399f03e6cfa07d8d32741e6244a5723131
Ade Akbar Magri Dinata <adeakbarmagridinata28@outlook.co.id>
2025-04-26 23:10:54 +0800

    Memasukkan global scope function kedalam objek, feedback sudah dapat disimpan oleh database

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const e = require('express');
 const express = require('express');
 const fs = require('fs');
+const { get } = require('http');
 
 const port = process.env.PORT || 3001;
 
@@ -10,62 +12,84 @@ app.use(express.urlencoded({extended: false}));
 const jsonFilePath = './data/database.json';
 const jsonDB = JSON.parse(fs.readFileSync(jsonFilePath));
 
-
-// console.log(getAvailableCode());
-
-function generateRandomDigit(){
-    let randomInt;
-    const existingCodes = getAvailableCode(); 
-    do {
-        randomInt = Math.floor(100000 + Math.random() * 900000);
-    } while (existingCodes.includes(randomInt));
-    return randomInt;
-};
-
 app.get('/', (req, res) => res.render('index'));
 
-app.get('new', (req, res) => res.render('new'));
+app.get('/new', (req, res) => res.render('new'));
 
 app.get('/join',(req, res) => res.render('join'));
 
+app.get('/dashboard', (req, res) => res.render('dashboard'));
+
 app.get('/feedback',(req, res) => {
     const code = parseInt(req.query.code);
-    if(!getAvailableCode().includes(parseInt(code))) return res.status(404).render('codenotfound', {code: code});
-    // console.log(`${code} is ${typeof(code)}`);
-    const eventIndex = jsonDB.events.findIndex(event => event.code === code);
-    const eventName = jsonDB.events[eventIndex].eventName
-    // console.log(eventIndex);
+    const eventIndex = getEvent.index(code);
+    const eventName = jsonDB.events[eventIndex].eventName;
+    if(!getEvent.availableCode().includes(parseInt(code))){
+        return res.status(404).render('codenotfound', {code: code});
+    };
     res.render('feedback', {eventName : eventName });
 });
 
-app.post('/checkcode', (req, res) => {
-    const code = req.body.code;
-    res.redirect(302, '/feedback?code=' + code);
-})
-
 app.post('/newsession', (req, res) => {
-    res.send('ok');
-    const data = generateEventJSON(req.body.eventName, req.body.password);
-    jsonDB.events.push(data)
+    res.redirect('/dashboard');
+    jsonDB.events.push(generate.eventJSON(req.body.eventName, req.body.password));
     fs.writeFile(jsonFilePath, JSON.stringify(jsonDB, null, 2), (err) => {if(err) console.log(err)});
 })
 
+app.post('/join', (req, res) => {
+    const code = req.body.code;
+    res.redirect('/feedback?code=' + code);
+})
 
-app.use((req, res) => res.status(404).render('404'))
+app.post('/feedback/send', (req, res) => {
+    res.send('Feedback submitted successfully! Thank you for your feedback!');
+    const code = req.query.code;
+    let name = req.body.name;
+    const eventIndex = getEvent.index(code);
+    if (req.body.name.length === 0) name = 'Anonymous';
+    const feedbackMessage = generate.eventFeedback(name, req.body.feedback);
+    jsonDB.events[eventIndex].feedback.push(feedbackMessage)
+    fs.writeFile(jsonFilePath, JSON.stringify(jsonDB, null, 2), (err) => {if(err) console.log(err)});
+    // console.log(req.body);
+    console.log(feedbackMessage);
+})
 
-function generateEventJSON(eventName, password) {
-    return {
-        eventName: eventName,
-        password: password,
-        code: generateRandomDigit(),
-        feedback: []
-    };
+app.use((req, res) => res.status(404).render('404'));
+
+const generate = {
+    eventJSON: function(eventName, password) {
+        return {
+            eventName: eventName,
+            password: password,
+            code: generate.eventCode(),
+            feedback: []
+        };
+    },
+    eventFeedback: function(name, feedback) {
+        return{
+            name: name,
+            feedback: feedback
+        }
+    },
+    eventCode: function() {
+        let randomInt;
+        const existingCodes = getEvent.availableCode(); 
+        do {
+            randomInt = Math.floor(100000 + Math.random() * 900000);
+        } while (existingCodes.includes(randomInt));
+        return randomInt;
+    }
 }
 
-function getAvailableCode(){
-    return jsonDB.events.map((event) => event.code)
-}
+const getEvent = {
+    index: function(code) {
+        return jsonDB.events.findIndex(event => event.code === parseInt(code));
+    },
+    availableCode(){
+        return jsonDB.events.map((event) => event.code);
+    }
 
+}
 
 app.listen(port, () => {
     console.log(`Aplikasi berjalan di http://localhost:${port}`);
