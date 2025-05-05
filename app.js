8e0ba463b8e6ce6c1524ceb7333de2f6d0e3e2b5
Ade Akbar Magri Dinata <adeakbarmagridinata28@outlook.co.id>
2025-05-05 20:39:18 +0800

    Memperbaiki tampilan feedback pada dashboard

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const fs = require('fs');
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
-const { error } = require('console');
 
 const port = process.env.PORT || 3000;
 
@@ -125,7 +124,7 @@ const generate = {
         }
     },
     eventFeedbackHtml: function(feedback, author){
-        return `<div class='feedback'><div class="feedback-content">${feedback}</div> <div class="feedback-author">${author}</div></div>`
+        return `<div class='feedback'><div class="feedback-content">${feedback}</div> <div class="feedback-author">Ditulis Oleh <span id='author'>${author}</span></div></div>`
     },
     errorHtml: function(errormessage){
         return `<div class="error"> <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg><p>${errormessage}</p></div>`
