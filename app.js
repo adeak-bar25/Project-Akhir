cadcb598d06bda816d6ac70f9de28a4005a19476
Ade Akbar Magri Dinata <adeakbarmagridinata28@outlook.co.id>
2025-05-08 00:33:56 +0800

    Menambahkan qrcode pada dashboard

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get('/dashboard', (req, res) => {
         let f = generate.eventFeedbackHtml(feedback.feedback, feedback.name)
         fArray.push(f)
     });
-    res.render('dashboard', {code: req.query.code, eventName: jsonDB.events[getEvent.index(req.query.code)].eventName, feedback: fArray.join(' ')});
+    res.render('dashboard', {code: req.query.code, eventName: jsonDB.events[getEvent.index(req.query.code)].eventName, fblength: jsonDB.events[i].feedback.length , feedback: fArray.join(' ')});
 });
 
 app.post('/newsession', (req, res) => {
@@ -129,7 +129,7 @@ const generate = {
         return `<div class='feedback'><div class="feedback-content">${feedback}</div> <div class="feedback-author">Ditulis Oleh <span id='author'>${author}</span></div></div>`
     },
     errorHtml: function(errormessage){
-        return `<div class="error"> <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg><p>${errormessage}</p></div>`
+        return `<div class="error"> <span style="color: #ea3323;" class="material-symbols-outlined">info</span><p>${errormessage}</p></div>`
     },
     eventCode: function() {
         let randomInt;
